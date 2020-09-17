0861e33b9b58eb6319a1bddd04e5d1016a8f19dd
JonatanHN <jonapato00@gmail.com>
2020-09-17 17:35:29 -0500

    make it better

--- OrionBOT.js
+++ OrionBOT.js
@@ -142,6 +142,24 @@ client.on("message", (message) => {
         });
     }
     // End JonatanHN 
+    //more commands
+    const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Some title')
+    .setURL('https://discord.js.org/')
+    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+    .setDescription('Some description here')
+    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+    .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+    )
+    .addField('Inline field title', 'Some value here', true)
+    .setImage('https://i.imgur.com/wSTFkRM.png')
+    .setTimestamp()
+    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
     // switch
     switch (command) {
         // Ping
@@ -217,6 +235,7 @@ client.on("message", (message) => {
              message.reply({embed: {
                 color: 3447003,
                 description: message.author.displayAvatarURL()
+                .setImage(message.author.displayAvatarURL())
              }});
             break;
     } //end switch
