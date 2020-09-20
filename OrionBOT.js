69ecae730bf5334afa865ac9b8f68b2c103ca983
JonatanHN <jonapato00@gmail.com>
2020-09-19 23:42:55 -0500

    make it better

--- OrionBOT.js
+++ OrionBOT.js
@@ -1,6 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const express = require('express');
+// Web
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
 var prefix = config.prefix;
 
 
